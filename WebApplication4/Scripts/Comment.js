0bae4c83721b7de0824158fc3885c7adba4dc5a7
keithst <keith.sturzenbecker@gmail.com>
2015-10-14 10:39:30 -0400

    Edit layout for comment section of blog

---
+++ WebApplication4/Scripts/Comment.js
@@ -0,0 +1,45 @@
+﻿
+function EditComment(inid) {
+    if (document.getElementById("B " + inid).hidden == true)
+    {
+        document.getElementById("B " + inid).hidden = false;
+    }
+    else
+    {
+        document.getElementById("B " + inid).hidden = true;
+    }
+
+    if (document.getElementById("T " + inid).hidden == true)
+    {
+        document.getElementById("T " + inid).hidden = false;
+    }
+    else
+    {
+        document.getElementById("T " + inid).hidden = true;
+    }
+
+    if (document.getElementById("U " + inid).hidden == true)
+    {
+        if (document.getElementById("U " + inid).innerText === "Update: ")
+        {
+            document.getElementById("U " + inid).hidden = true;
+        }
+        else
+        {
+            document.getElementById("U " + inid).hidden = false;
+        }
+    }
+    else
+    {
+        document.getElementById("U " + inid).hidden = true;
+    }
+
+    if (document.getElementById("UE " + inid).hidden == true)
+    {
+        document.getElementById("UE " + inid).hidden = false;
+    }
+    else
+    {
+        document.getElementById("UE " + inid).hidden = true;
+    }
+}
