0c570d61a1cea63e76fdb7bb79f6c0b8cccb21a2
keithst <keith.sturzenbecker@gmail.com>
2015-10-19 14:10:41 -0400

    Working contact page

--- WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using SendGrid;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,6 +27,29 @@ namespace WebApplication4.Controllers
         public ActionResult Contact()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(string From, string Subject, string Body)
+        {
+            var username = ConfigurationManager.AppSettings["SendGridUserName"];
+            var password = ConfigurationManager.AppSettings["SendGridPassword"];
+            var from = ConfigurationManager.AppSettings["ContactEmail"];
+
+            SendGridMessage myMessage = new SendGridMessage();
+            myMessage.AddTo("keith.sturzenbecker@gmail.com");
+            myMessage.From = new MailAddress(From);
+            myMessage.Subject = Subject;
+            myMessage.Html = Body;
+
+            var credentials = new NetworkCredential(username, password);
+
+            var transportWeb = new Web(credentials);
+
+            transportWeb.DeliverAsync(myMessage);
+
+            return RedirectToAction("Contact");
         }
 
         public ActionResult Resume()
